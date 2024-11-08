cb69ec7836234e10f0b3a35f93feb4cfcee4a36e
M <115192833+Melissax0@users.noreply.github.com>
2024-11-08 17:28:53 -0500

    finished Meatzza and BuildYourOwn

--- src/main/java/projects/rupizzeria/rupizzeria/BuildYourOwn.java
+++ src/main/java/projects/rupizzeria/rupizzeria/BuildYourOwn.java
@@ -1,7 +1,45 @@
 package projects.rupizzeria.rupizzeria;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class BuildYourOwn extends Pizza{
+    private final double SMALL_PRICE = 8.99;
+    private final double MED_PRICE = 10.99;
+    private final double LARGE_PRICE = 12.99;
+    private final double TOPPING_PRICE = 1.69;
+    private final int MAX_TOPPINGS = 7;
+    /**
+     * Default constructor for BuildYourOwn pizza.
+     */
+    public BuildYourOwn() {
+        super();
+        super.setToppings(new ArrayList<>());
+        super.setCrust(Crust.PAN);
+        super.setSize(Size.SMALL);
+    }
+
+    /**
+     * Add a topping to BuildYourOwn pizza if there are less than 7 toppings on the pizza.
+     * @param topping topping to be added to the pizza
+     */
+    public void addTopping(Topping topping) {
+        if (super.getToppings().size() < MAX_TOPPINGS)
+            super.getToppings().add(topping);
+    }
+
+    /**
+     * Calculates the price of the BuildYourOwn Pizza.
+     * @return price of the BuildYourOwn Pizza.
+     */
     public double price() {
-        return 0;
+        double toppingsPrice = super.getToppings().size() * TOPPING_PRICE;
+        switch(super.getSize()) {
+            case SMALL -> {return SMALL_PRICE + toppingsPrice;}
+            case MEDIUM -> {return MED_PRICE + toppingsPrice;}
+            case LARGE -> {return LARGE_PRICE + toppingsPrice;}
+            default -> {return 0;}
+        }
     }
 }
---
+++ src/main/java/projects/rupizzeria/rupizzeria/OrderController.java
@@ -0,0 +1,5 @@
+package projects.rupizzeria.rupizzeria;
+
+public class OrderController {
+
+}
