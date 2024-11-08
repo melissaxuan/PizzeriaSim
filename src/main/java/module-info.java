44871cd6d0707b167590bc8a755840008a2f4d2e
Michaele54 <mje121@scarletmail.rutgers.edu>
2024-11-08 18:26:53 -0500

    menu page

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module projects.rupizzeria.rupizzeria {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens projects.rupizzeria.rupizzeria to javafx.fxml;
--- src/main/java/projects/rupizzeria/rupizzeria/MainController.java
+++ src/main/java/projects/rupizzeria/rupizzeria/MainController.java
@@ -1,14 +1,29 @@
 package projects.rupizzeria.rupizzeria;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+
+import javax.swing.text.html.ImageView;
 
 public class MainController {
     @FXML
     private Label welcomeText;
 
     @FXML
+    private ImageView IV_OrderPLaced;
+
+    @FXML
+    private Button B_OrderPlaced;
+
+    @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void initialize(){
+
     }
 }
