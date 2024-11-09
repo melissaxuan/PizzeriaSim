9011f9e5bb23f70b10cb5084f66cfc159a586946
Michaele54 <mje121@scarletmail.rutgers.edu>
2024-11-08 20:44:13 -0500

    GUI for menu and order place

---
+++ src/main/java/projects/rupizzeria/rupizzeria/BillViewController.java
@@ -0,0 +1,33 @@
+package projects.rupizzeria.rupizzeria;
+
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.scene.control.ListView;
+
+
+public class BillViewController {
+
+
+    @FXML
+    private ListView lv_bill;
+    @FXML
+    private Button B_cancelOrder;
+    @FXML
+    private Button B_exportStore;
+
+
+    private MainController mainController;
+    private Stage stage;
+    private Scene primaryScene;
+    private Stage primaryStage;
+    private ObservableList<String> colorList, fruitList, peopleList;
+    public void setMainController (MainController controller, Stage stage, Stage primaryStage, Scene primaryScene) {
+        mainController = controller;
+        this.stage = stage;
+        this.primaryStage = primaryStage;
+        this.primaryScene = primaryScene;
+    }
+}
